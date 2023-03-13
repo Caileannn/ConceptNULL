5c9de346f07c2acd0ae44fbe6822ef1be9d725d8
Cailean Finn <Caileannn@gmail.com>
2023-03-13 11:50:36 +0000

    Emojis

--- src/components/Newbar/index.js
+++ src/components/Newbar/index.js
@@ -11,11 +11,11 @@ export default function Index() {
     <div className="nb-band">
         <div className="btn-band">
             <div className="btn-group">
-                <Link to="/news"><div className="btn-nav">News<span className="span-uni-arrow">&#129130;</span></div></Link>
-                <Link to="/about"><div className="btn-nav">About<span className="span-uni-arrow">&#x1F86D;</span></div></Link>
+                <Link to="/news"><div className="btn-nav">News<span className="span-uni-arrow">📰</span></div></Link>
+                <Link to="/about"><div className="btn-nav">About<span className="span-uni-arrow">👋</span></div></Link>
                 <Link to="/" style={{textDecoration:'none' }}><div className="btn-symbol">ø</div></Link>
-                <Link to="/faq"><div className="btn-nav">F.A.Q<span className="span-uni-arrow">&#x1F86D;</span></div></Link> 
-                <Link to="/"><div className="btn-nav">#<span className="span-uni-arrow">&#x1F86D;</span></div></Link>
+                <Link to="/faq"><div className="btn-nav">F.A.Q<span className="span-uni-arrow">🙋‍♀️</span></div></Link> 
+                <Link to="/"><div className="btn-nav">#<span className="span-uni-arrow">🧭</span></div></Link>
             </div>
         </div>
     </div>
