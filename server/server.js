8ea1142e0733b84760d904a7f810447746265f64
Cailean Finn <Caileannn@gmail.com>
2023-03-27 23:57:29 +0100

    Small

--- server/server.js
+++ server/server.js
@@ -20,28 +20,28 @@ app.get('/newsletter-introduction', async(req, res) => {
      res.setHeader('Content-Type', 'application/json');
      const introduction = await getIntro()
      console.log("<Introduction Fetched>")
-     res.json(introduction)
+     return res.json(introduction)
 })
 
 app.get('/updates', async(req, res) => {
      res.setHeader('Content-Type', 'application/json');
      const update = await getUpdate()
      console.log("<Updated Fetched>")
-     res.json(update)
+     return res.json(update)
 })
 
 app.get('/spotlights', async(req, res) => {
      res.setHeader('Content-Type', 'application/json');
      const spotlight = await getSpotlight()
      console.log("<Spotlight Fetched>")
-     res.json(spotlight)
+     return res.json(spotlight)
 })
 
 app.get('/calls', async(req, res) => {
      res.setHeader('Content-Type', 'application/json');
      const calls = await getCalls()
      console.log("<Calls Fetched>")
-     console.log(res.json(calls))
+     return res.json(calls)
 })
 
 
