143ff985a73487224a387c2bd1aee18d9ba6ee82
Cailean Finn <Caileannn@gmail.com>
2023-03-27 23:36:13 +0100

    header update_03

--- server/server.js
+++ server/server.js
@@ -17,30 +17,30 @@ const app = express()
 
 // Newsletter Call
 app.get('/newsletter-introduction', async(req, res) => {
+     res.setHeader('Content-Type', 'application/json');
      const introduction = await getIntro()
      console.log("<Introduction Fetched>")
-     res.setHeader('Content-Type', 'application/json');
      res.json(introduction)
 })
 
 app.get('/updates', async(req, res) => {
+     res.setHeader('Content-Type', 'application/json');
      const update = await getUpdate()
      console.log("<Updated Fetched>")
-     res.setHeader('Content-Type', 'application/json');
      res.json(update)
 })
 
 app.get('/spotlights', async(req, res) => {
+     res.setHeader('Content-Type', 'application/json');
      const spotlight = await getSpotlight()
      console.log("<Spotlight Fetched>")
-     res.setHeader('Content-Type', 'application/json');
      res.json(spotlight)
 })
 
 app.get('/calls', async(req, res) => {
+     res.setHeader('Content-Type', 'application/json');
      const calls = await getCalls()
      console.log("<Calls Fetched>")
-     res.setHeader('Content-Type', 'application/json');
      res.json(calls)
 })
 
