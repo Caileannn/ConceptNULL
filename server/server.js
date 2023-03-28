006e21ee30238179206056f5666a56be1f16e3e1
Cailean Finn <Caileannn@gmail.com>
2023-03-28 20:59:27 +0100

    update

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const routeHandler = require('./services/notion.js')
 const cors = require('cors')
-const port = process.env.PORT 
+const port = 10000;
 
 
 
