26998596736b022042f3508864df41834d18b728
Cailean Finn <Caileannn@gmail.com>
2023-03-28 12:15:53 +0100

    Server H

--- server/server.js
+++ server/server.js
@@ -25,29 +25,42 @@ app.use(function(req, res, next) {
 
 // Newsletter Call
 app.get('/newsletter-introduction', async(req, res) => {
-     res.setHeader('Content-Type', 'application/json');
+     res.header("Access-Control-Allow-Origin", '*');
+     res.header("Access-Control-Allow-Credentials", true);
+     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
      const introduction = await getIntro()
      console.log("<Introduction Fetched>")
      return res.json(introduction)
 })
 
 app.get('/updates', async(req, res) => {
-     res.setHeader('Content-Type', 'application/json');
+     res.header("Access-Control-Allow-Origin", '*');
+     res.header("Access-Control-Allow-Credentials", true);
+     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
      const update = await getUpdate()
      console.log("<Updated Fetched>")
      return res.json(update)
 })
 
 app.get('/spotlights', async(req, res) => {
-     res.setHeader('Content-Type', 'application/json');
+     res.header("Access-Control-Allow-Origin", '*');
+     res.header("Access-Control-Allow-Credentials", true);
+     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
      const spotlight = await getSpotlight()
      console.log("<Spotlight Fetched>")
      return res.json(spotlight)
 })
 
 app.get('/calls', async(req, res) => {
-     res.setHeader('Content-Type', 'application/json');
+     res.header("Access-Control-Allow-Origin", '*');
+     res.header("Access-Control-Allow-Credentials", true);
+     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
      const calls = await getCalls()
+     console.log(calls)  
      console.log("<Calls Fetched>")
      return res.json(calls)
 })
